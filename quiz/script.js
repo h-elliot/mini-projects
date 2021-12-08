f2cc62fe243652bd21a3f4c45742abc32dd3b747
h-elliot <hunter.e.canavan@gmail.com>
2021-12-08 15:34:52 -0500

    basic file set up

---
+++ quiz/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+	{
+		question: 'What is the best flavor?',
+		a: 'Strawberry',
+		b: 'Mint',
+		c: 'Peanut butter',
+		d: 'Vanilla',
+		correct: 'c',
+	},
+	{
+		question: 'What is the capital of New York?',
+		a: 'New York City',
+		b: 'Albuquerque',
+		c: 'Gotham City',
+		d: 'Albany',
+		correct: 'd',
+	},
+	{
+		question: 'What is the coolest kind of tree?',
+		a: 'Giant Redwood',
+		b: 'White Birch',
+		c: 'Ebony',
+		d: 'A dead tree',
+		correct: 'b',
+	},
+	{
+		question: 'Which berry is the largest?',
+		a: 'Pumpkin',
+		b: 'Cucumber',
+		c: 'Dragonfruit',
+		d: 'Blueberry',
+		correct: 'a',
+	},
+];
